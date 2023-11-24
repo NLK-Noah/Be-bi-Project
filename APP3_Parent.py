c2c39df8bd924b429b575a89bdc13707c18c720b
Moussaoui noah <128749614+NLK-Noah@users.noreply.github.com>
2023-11-24 11:55:33 +0100

    Update APP3_Parent.py
    compteur de lait mise au point avant la save de la variable count

--- APP3_Parent.py
+++ APP3_Parent.py
@@ -1,3 +1,4 @@
+#Code pour le BEtag des parents
 from microbit import *
 import radio
 import music
@@ -6,18 +7,53 @@ import time
 radio.config(group=22)
 radio.on()
 
-for x in range(2):
-    music.play(["C4:4", "D4", "E4", "C4"])
+    
+def start():
+    for x in range(2):
+        music.play(["C4:4", "D4", "E4", "C4"])
+    for x in range(2):
+        music.play(["E4:4", "F4", "G4:8"])
+        sleep(5)
 
-for x in range(2):
-    music.play(["E4:4", "F4", "G4:8"])
-display.scroll("Bienvenue sur Be:TAG")
-sleep(5)
-
-while True:
-    display.scroll("Press A for settings")
+def command():
     if button_a.was_pressed():
-        display.clear()
-        sleep(1000)
-        break
+        setting()
+
+
+def menu():
+    while True :
+        display.show(Image.DUCK)
+        command()
+        if command():
+            break
+
+count = 0
+def setting():
+    global count
+
+    display.scroll("Launch of the milk counter")
+    display.show(0)
+    while True:
+        if button_b.is_pressed():
+            count += 1
+            display.show(count)
+            sleep(500)
+        if button_a.is_pressed():
+            count -= 1
+            display.show(count)
+            sleep(500)
+        if accelerometer.was_gesture('shake'):
+            count = 0
+            display.show(count)
+            sleep(500)
+        if pin_logo.is_touched():
+            menu()
+
+
+def main():
+    start()
+    menu()
+
+if __name__ == "__main__":
+    main()
 
